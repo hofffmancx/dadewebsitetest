736ae1b1275a70a67f64dba138c0f40f48da2a0b
罗泷 <godluos@luolongdeMacBook-Air.local>
2017-05-14 17:49:22 +0800

    add admin to project

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+a = Admin.new
+a.email = "admin@ddtc.com"           
+a.password = "123456"
+a.password_confirmation = "123456"
+a.save
