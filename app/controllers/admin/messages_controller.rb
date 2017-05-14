f116cb32eb39ea68ab4549931bb661e858ddc837
罗泷 <godluos@luolongdeMacBook-Air.local>
2017-05-15 00:31:57 +0800

    add message to project

---
+++ app/controllers/admin/messages_controller.rb
@@ -0,0 +1,49 @@
+class Admin::MessagesController < ApplicationController
+  before_action :authenticate_admin!
+
+  def index
+    @messages = Message.all
+  end
+
+  def show
+    @message = Message.find(params[:id])
+  end
+
+  def new
+    @message = Message.new
+  end
+
+  def create
+    @message = Message.new(message_params)
+    if @message.save
+      redirect_to admin_messages_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @message = Message.find(params[:id])
+  end
+
+  def update
+    @message = Message.find(params[:id])
+    if @message.update(message_params)
+      redirect_to admin_messages_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @message = Message.find(params[:id])
+    @message.destroy
+    redirect_to admin_messages_path
+  end
+
+  private
+
+    def message_params
+      params.require(:message).permit(:title, :description,)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :admin do
     resources :products
+    resources :messages
   end
 
   resources :products
+  resources :messages
 
   root "welcome#index"
 end
